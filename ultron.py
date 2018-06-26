ee8a36cfc93ba0e06147569b0e0e5e771daf47b7
fragmentsbot <40477048+fragmentsbot@users.noreply.github.com>
2018-06-26 17:14:10 +0200

    Update ultron.py

--- ultron.py
+++ ultron.py
@@ -14,7 +14,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if not message.author.bot and (message.server == None or client.user in message.mentions):
+  if message.author.id == "461174437627166731":
+        return
+    else:
         await client.send_typing(message.channel)
         txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
         r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
